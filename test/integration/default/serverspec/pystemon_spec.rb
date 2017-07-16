8cea28af66439f46f658c79a83dcdb510d5edfa3
juju4 <juju4@users.noreply.github.com>
2017-07-16 07:31:31 -0400

    add python deps serverspec test

--- test/integration/default/serverspec/pystemon_spec.rb
+++ test/integration/default/serverspec/pystemon_spec.rb
@@ -13,6 +13,14 @@ describe file('/var/_pystemon/pystemon/pystemon.py') do
   it { should be_executable }
 end
 
+describe command('pip freeze') do
+  its(:stdout) { should match /PyYAML/ }
+  its(:stdout) { should match /redis/ }
+  its(:stdout) { should match /requests/ }
+  its(:stdout) { should match /urllib/ }
+  its(:exit_status) { should eq 0 }
+end
+
 describe command('/var/_pystemon/pystemon/pystemon.py -h') do
   its(:stdout) { should match /Usage: pystemon.py/ }
   its(:exit_status) { should eq 0 }
